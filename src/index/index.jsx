c288b84d5fb7c9dcd602d2a79b57574d900a40bf
lungaHamilton09 <141244743+lungaHamilton09@users.noreply.github.com>
2024-02-08 11:18:35 +0200

    Added Index,js and index,jsx for the homepage

--- src/index/index.jsx
+++ src/index/index.jsx
@@ -1,71 +1,73 @@
+import React from "react";
+import "./style.css";
 
 export const Index = () => {
-    return (
-      <div className="index">
-        <div className="overlap-group-wrapper">
-          <div className="overlap-group">
-            <div className="overlap">
-              <img
-                className="rectangle"
-                alt="Rectangle"
-                src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/rectangle-13.svg"
-              />
-              <img
-                className="img"
-                alt="Rectangle"
-                src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/rectangle-11.svg"
-              />
-              <img
-                className="rectangle-2"
-                alt="Rectangle"
-                src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/rectangle-12.svg"
-              />
-              <div className="group">
-                <div className="group-wrapper">
-                  <div className="div-wrapper">
-                    <div className="div">
-                      <div className="text-wrapper">Lunga Momoza</div>
-                      <div className="text-wrapper-2">About</div>
-                      <div className="text-wrapper-3">Experience</div>
-                      <div className="text-wrapper-4">Projects</div>
-                      <div className="text-wrapper-5">Contact</div>
-                    </div>
+  return (
+    <div className="index">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <div className="overlap">
+            <img
+              className="rectangle"
+              alt="Rectangle"
+              src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/rectangle-13.svg"
+            />
+            <img
+            className="img"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/rectangle-11.svg"
+            />
+            <img
+            className="rectangle-2"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/rectangle-12.svg"
+            />
+            <div className="group">
+              <div className="group-wrapper">
+                <div className="div-wrapper">
+                  <div className="div">
+                    <div className="text-wrapper">Lunga Momoza</div>
+                    <div className="text-wrapper-2">About</div>
+                    <div className="text-wrapper-3">Experience</div>
+                    <div className="text-wrapper-4">Project</div>
+                    <div className="text-wrapper-5">Contact</div>
                   </div>
                 </div>
               </div>
-              <div className="text-wrapper-6">Hello, I’m</div>
-              <div className="text-wrapper-7">Lunga Momoza</div>
-              <div className="text-wrapper-8">Software Developer</div>
-              <img
-                className="ellipse"
-                alt="Ellipse"
-                src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/ellipse-1@2x.png"
-              />
-              <div className="download-CV">Download&nbsp;&nbsp;CV</div>
-              <div className="text-wrapper-9">Contact Info</div>
-              <img
-                className="download-icon"
-                alt="Download icon"
-                src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/download-icon-1@2x.png"
-              />
             </div>
+            <div className="text-wrapper-6">Hello, I'M</div>
+            <div className="text-wrapper-7">Lunga Momoza</div>
+            <div className="text-wrapper-8">Software Developer</div>
             <img
-              className="brown-linkedln-icon"
-              alt="Brown linkedln icon"
-              src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/brown-linkedln-icon-1.png"
+            className="ellipse"
+            alt="Ellipse"
+            src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/ellipse-1@2x.png"
             />
+            <div className="download-CV">Download&nbsp;&nbsp;CV</div>
+            <div className="text-wrapper-9">Contact Info</div>
             <img
-              className="brown-email-icon"
-              alt="Brown email icon"
-              src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/brown-email-icon-1.png"
-            />
-            <img
-              className="github-brown-icon"
-              alt="Github brown icon"
-              src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/github-brown-icon-1.png"
+            className="download-icon"
+            alt="Download icon"
+            src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/download-icon-1@2x.png"
             />
           </div>
+          <img
+          className="brown-linkedln-icon"
+          alt="Download icon"
+          src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/brown-linkedln-icon-1.png"
+          />
+          <img 
+          className="brown-email-icon"
+          alt="Brown email icon"
+          src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/brown-email-icon-1.png"
+          />
+          <img
+          className="github-brown-icon"
+          alt="Github brown icon"
+          src="https://cdn.animaapp.com/projects/65c220565c16bd6d7dee3530/releases/65c220a9e07f12a1ee39384a/img/github-brown-icon-1.png"
+          />
         </div>
       </div>
-    );
-  };
+    </div>
+  )
+}
